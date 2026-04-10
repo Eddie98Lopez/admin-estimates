a414ed3fcb5ee2a7e12e03c2659c2f4fc252f166
Eduardo Lopez <ed98lopez@gmail.com>
2026-04-10 15:49:23 -0700

    wow didnt know so many things default to on

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,6 +14,19 @@ const eslintConfig = [
   {
     rules: {
       '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      'prefer-const': 'off',
+      '@typescript-eslint/no-empty-object-type': 'off',
+      '@typescript-eslint/no-unsafe-function-type': 'off',
+      '@typescript-eslint/no-wrapper-object-types': 'off',
+      '@typescript-eslint/ban-ts-comment': 'off',
+      '@typescript-eslint/no-require-imports': 'off',
+      'react/no-unescaped-entities': 'off',
+      'react-hooks/exhaustive-deps': 'off',
+      'import/no-anonymous-default-export': 'off',
+      '@next/next/no-img-element': 'off',
+      'no-console': 'off',
+      'no-unused-expressions': 'off',
     },
   },
 ];
